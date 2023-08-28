89196fb09e0fd5bbca75c6a7ef3e1cbb4d81e8b3
Iurii Pototskii <iuriipototskii2gmail.com>
2023-08-28 11:47:08 +0200

    feat: list sort - deleted last

--- src/widgets/catalog/model/catalog-selectors.ts
+++ src/widgets/catalog/model/catalog-selectors.ts
@@ -3,7 +3,24 @@ import { NameSpace, State } from '../../../app/provider/store';
 import { StoredGoodType } from '../../../entities/good';
 import { FetchStatus } from '../../../shared/types/fetch-status';
 
-export const getCatalog = ( state: State ): StoredGoodType[] => state[NameSpace.Catalog].catalog;
+export const getCatalog = createSelector(
+  ( state: State ): StoredGoodType[] => state[NameSpace.Catalog].catalog,
+  ( catalog ) => [...catalog].sort( ( a, b ) => {
+    if ( ( a.removed && b.removed ) || ( !a.removed && !b.removed ) ) {
+      return 0;
+    }
+
+    if ( a.removed && !b.removed ) {
+      return 1;
+    }
+
+    if ( !a.removed && b.removed ) {
+      return -1;
+    }
+
+    return 0;
+  } )
+);
 
 export const getCatalogLoadingStatus = createSelector(
   ( state: State ): FetchStatus => state[NameSpace.Catalog].catalogLoadingStatus,
